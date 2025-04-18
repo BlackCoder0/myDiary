b99cfb2484c1dee85f677669af0b7dc6623347ef
Code-Liang <2943984952@qq.com>
2025-04-19 04:54:31 +0800

    重构完成！bug全部修复！！！

--- app/src/main/java/com/code/mydiary/ContentFragment.java
+++ app/src/main/java/com/code/mydiary/ContentFragment.java
@@ -14,7 +14,6 @@ public class ContentFragment extends Fragment {
     private static final String TAG = "ContentFragment";
     private static final String ARG_PAGE_TYPE = "page_type";
     private View rootView;
-    private ImageButton imgBtMenu, imgBtAdd;
 
     public static ContentFragment newInstance(int pageType) {
         ContentFragment fragment = new ContentFragment();
@@ -24,58 +23,27 @@ public class ContentFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        int pageType = getArguments().getInt(ARG_PAGE_TYPE, 0);
-        View view = null;
-
-        switch (pageType) {
-            case 0: // Entries 页面
-                view = inflater.inflate(R.layout.select_by_text_activity, container, false);
-                imgBtMenu = view.findViewById(R.id.imgBt_menu);
-                imgBtAdd = view.findViewById(R.id.imgBt_add);
-
-                // 设置按钮的点击事件监听器
-                if (imgBtMenu != null) {
-                    imgBtMenu.setOnClickListener(v -> {
-                        // 跳转到 MenuActivity
-                        Toast.makeText(getActivity(), "点击了菜单按钮2", Toast.LENGTH_SHORT).show();
-                        Intent menuIntent = new Intent(getActivity(), Menu.class);
-                        startActivity(menuIntent);
-                    });
-                }
-
-                if (imgBtAdd != null) {
-                    imgBtAdd.setOnClickListener(v -> {
-                        // 跳转到 EditDiaryActivity
-                        Toast.makeText(getActivity(), "点击了日记按钮2", Toast.LENGTH_SHORT).show();
-                        Intent addIntent = new Intent(getActivity(), EditDiary.class);
-                        startActivity(addIntent);
-                    });
-                }
-                break;
-            case 1: // Calendar 页面
-                view = inflater.inflate(R.layout.select_by_day_activity, container, false);
-                break;
-            case 2: // Dairy 页面
-                view = inflater.inflate(R.layout.setting_activity, container, false);
-                break;
-            default:
-                view = inflater.inflate(R.layout.select_by_text_activity, container, false);
-                imgBtMenu = view.findViewById(R.id.imgBt_menu);
-                imgBtAdd = view.findViewById(R.id.imgBt_add);
-                break;
-        }
-
-        rootView = view;
-        return view;
-    }
-
-    public ImageButton getImgBtMenu() {
-        return imgBtMenu;
-    }
-
-    public ImageButton getImgBtAdd() {
-        return imgBtAdd;
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        int pageType = getArguments().getInt(ARG_PAGE_TYPE, 0);
+//        View view = null;
+//
+//        switch (pageType) {
+//            case 0: // Entries 页面
+//                view = inflater.inflate(R.layout.select_by_text_activity, container, false);
+//                break;
+//            case 1: // Calendar 页面
+//                view = inflater.inflate(R.layout.select_by_day_activity, container, false);
+//                break;
+//            case 2: // Dairy 页面
+//                view = inflater.inflate(R.layout.setting_activity, container, false);
+//                break;
+//            default:
+//                view = inflater.inflate(R.layout.select_by_text_activity, container, false);
+//                break;
+//        }
+//
+//        rootView = view;
+//        return view;
+//    }
 }
--- app/src/main/java/com/code/mydiary/MainActivity.java
+++ app/src/main/java/com/code/mydiary/MainActivity.java
@@ -2,23 +2,41 @@ package com.code.mydiary;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import com.code.mydiary.util.TabLayout;
+import com.code.mydiary.util.TabPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    final String TAG = "test";
     private TextView tabEntries, tabCalendar, tabDiary;
+    private ImageButton myBtnMenu, myBtnAdd;
     private FrameLayout containerEntries,containerCalendar, containerDiary;
+    private ViewPager viewPager;
 
-    @SuppressLint("WrongViewCast")
+    private ContentFragment currentFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main_activity);
+//        Intent intent = new Intent(this, SelectByText.class);
+//        startActivity(intent);
+//        finish();
 
         // 初始化 tab 控件
         tabEntries = findViewById(R.id.tab_entries);
@@ -37,6 +55,51 @@ public class MainActivity extends Activity {
 
         // 默认显示 Entries 页面
         switchPage(0);
+
+        // 初始化 Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().setDisplayShowTitleEnabled(false);
+            }
+        }
+
+//        // 初始化标签
+//        tabEntries = findViewById(R.id.tab_entries);
+//        tabCalendar = findViewById(R.id.tab_calendar);
+//        tabDairy = findViewById(R.id.tab_diary);
+
+        // 初始化 ViewPager
+        viewPager = findViewById(R.id.view_pager);
+        if (viewPager != null) {
+            TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager());
+            viewPager.setAdapter(adapter);
+            viewPager.setOffscreenPageLimit(0); // 禁用缓存
+
+            // 设置 ViewPager 的页面变化监听器
+            viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+                @Override
+                public void onPageSelected(int position) {
+                    updateCurrentFragment(position);
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {}
+            });
+        }
+
+//        // 初始化 TabLayout
+//        if (tabEntries != null && tabCalendar != null && tabDiary != null && viewPager != null) {
+//            TabLayout tabLayout = new TabLayout(tabEntries, tabCalendar, tabDiary, viewPager);
+//            tabLayout.initTabs();
+//        }
+
+        // 设置按钮的点击事件
+        setButtonListeners();
     }
 
     private void switchPage(int index) {
@@ -55,4 +118,47 @@ public class MainActivity extends Activity {
         else if (index == 2) tabDiary.setSelected(true);
     }
 
+
+private void updateCurrentFragment(int position) {
+    if (viewPager != null) {
+        currentFragment = (ContentFragment) viewPager.getAdapter().instantiateItem(viewPager, position);
+    }
+}
+    private void setButtonListeners() {
+        myBtnMenu = findViewById(R.id.imgBt_menu);
+        myBtnAdd = findViewById(R.id.imgBt_add);
+
+        if (myBtnMenu != null) {
+            myBtnMenu.setOnClickListener(v -> {
+                Log.d("ClickDebug", "Menu按钮被点击");
+                // 跳转到 MenuActivity
+                Intent menuIntent = new Intent(MainActivity.this, Menu.class);
+                startActivity(menuIntent);
+            });
+        }
+
+        if (myBtnAdd != null) {
+            myBtnAdd.setOnClickListener(v -> {
+                Log.d("ClickDebug", "Add按钮被点击");
+                // 跳转到 EditDiaryActivity
+                Intent addIntent = new Intent(MainActivity.this, EditDiary.class);
+                startActivityForResult(addIntent, 0);
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            String addDiary = data.getStringExtra("input");
+            if (addDiary != null) {
+                Log.d(TAG, "收到的日记内容: " + addDiary);
+            } else {
+                Log.d(TAG, "收到的日记内容为空");
+            }
+        } else {
+            Log.d(TAG, "未收到有效的返回结果");
+        }
+    }
 }
